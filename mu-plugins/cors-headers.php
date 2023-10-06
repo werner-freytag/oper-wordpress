680dd73aba9fcb6350dbc80e27236da8adca440c
Elias <e.arnold@soul-surf.com>
2023-10-06 11:10:16 +0200

    Update JWT Auth

--- mu-plugins/cors-headers.php
+++ mu-plugins/cors-headers.php
@@ -10,7 +10,7 @@ function initCors( $value ) {
 
   header( 'Access-Control-Allow-Origin: *' );
   // header( 'Access-Control-Allow-Origin: ' . $origin_url );
-  header( 'Access-Control-Allow-Methods: GET' );
+  header( 'Access-Control-Allow-Methods: GET, POST' );
   header( 'Access-Control-Allow-Credentials: true' );
   return $value;
 }
@@ -20,6 +20,8 @@ add_action( 'rest_api_init', function() {
   remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
   add_filter( 'rest_pre_serve_request', 'initCors');
 }, 15 );
+
+
 
 // function add_cors_http_header(){
 //     header("Access-Control-Allow-Origin: *");
