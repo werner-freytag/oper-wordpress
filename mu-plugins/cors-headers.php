1db79869cfff71b977d8df866ddeb0c3ae7b5223
Werner Freytag <freytag@gmx.de>
2024-09-13 08:36:47 +0200

    allow Access-Control-Allow-Headers header

--- mu-plugins/cors-headers.php
+++ mu-plugins/cors-headers.php
@@ -13,7 +13,11 @@ function initCors( $value ) {
   // header( 'Access-Control-Allow-Origin: ' . $origin_url );
   header( 'Access-Control-Allow-Methods: GET, POST' );
   header( 'Access-Control-Allow-Credentials: true' );
-  return $value;
+
+    // add X-Cache-Variant
+    header( 'Access-Control-Allow-Headers: Authorization, X-WP-Nonce, Content-Disposition, Content-MD5, Content-Type, X-Cache-Variant');
+
+    return $value;
 }
 
 add_action( 'rest_api_init', function() {
@@ -21,8 +25,6 @@ add_action( 'rest_api_init', function() {
   remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
   add_filter( 'rest_pre_serve_request', 'initCors');
 }, 15 );
-
-
 
 // function add_cors_http_header(){
 //     header("Access-Control-Allow-Origin: *");
